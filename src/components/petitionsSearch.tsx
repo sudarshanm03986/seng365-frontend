29ab875da3fe76085b846ead3cc678648b7ba810
Sudarshan Malla <48778320+sudarshanm03986@users.noreply.github.com>
2024-04-06 10:06:55 +1300

    idk

--- src/components/petitionsSearch.tsx
+++ src/components/petitionsSearch.tsx
@@ -7,7 +7,7 @@ const PetitionsSearch = (props:any) => {
 
 
     return ( 
-
+    
         <input placeholder="Search Petitions" value={searchParams.get('q')?.toString()} onChange={e=> setSearchParams({'q' : e.target.value})}/>
         
      );
